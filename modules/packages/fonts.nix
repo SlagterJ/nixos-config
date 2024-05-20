a43203edbf9687d6798b9df0f7511168873f24e7
Jordy Slagter <slagterj2001@gmail.com>
2024-05-20 12:35:31 +0200

    add fantasquesansmono nerd font

--- modules/packages/fonts.nix
+++ modules/packages/fonts.nix
@@ -1,10 +1,16 @@
+# References:
+# - https://nixos.wiki/wiki/Fonts
+
+
 { config, pkgs, ... }:
 
 
 {
-  environment.systemPackages = with pkgs; [
+  fonts.packages = with pkgs; [
     noto-fonts
     noto-fonts-color-emoji
     noto-fonts-cjk-sans
+
+    (nerdfonts.override { fonts = [ "FantasqueSansMono" ]; })
   ];
 }
