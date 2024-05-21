c5a4016ca541f29a300d5601b6d4f5cc67040d8f
Jordy Slagter <slagterj2001@gmail.com>
2024-05-21 13:38:03 +0200

    add javascript

--- modules/packages/development.nix
+++ modules/packages/development.nix
@@ -3,13 +3,21 @@
 
 {
   environment.systemPackages = with pkgs; [
+    # essentials
     git
     gh
     neovim
     vscode
+
+    # infrastructure
     filezilla
     tshark
     wireshark
+
+    # javascript
+    nodejs
+    yarn
+    typescript
   ];
 
   virtualisation.docker.enable = true;
